5d390d01bcaae6959d30e595dc022a483927e38b
Greg Schulte <rikkitikkitumbo@gmail.com>
2014-05-29 12:00:25 -0700

    firstCommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Greg2::Application.config.secret_key_base = '769024d6300de3a152f38b606765514c7efd023b57efde4b5e80d08a8da492a7d4c93b1a5f80f171f64bcef509bf82686706bfdc1973859bca5deb2a1112c90a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Greg2::Application.config.session_store :cookie_store, key: '_Greg2_session'
